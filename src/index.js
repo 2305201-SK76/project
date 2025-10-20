798f33a185828607298a105a17d6a826d5868831
2305201-SK76 <2305201.rcca@gmail.com>
2025-10-20 17:52:48 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const {add , subtract,multiply} = require("./math");
+
+console.log("Additon", add(5,10));
+console.log("Subtraction", subtract(5,10));
+console.log("Multiplication", multiply(5,10));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
